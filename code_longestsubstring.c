f5b7301c9002cbe3f421cb392e54b3ffd85d3646
neenadthite <neenadthite@outlook.com>
2026-05-08 15:52:49 +0530

    String: modified source code after debugging

--- code_longestsubstring.c
+++ code_longestsubstring.c
@@ -1,25 +1,45 @@
+#include <string.h>
+#include <iostream.h>
+
+
+int lengthOfLongestSubstring(char*);
+int unq_ptrn(char*, char, int);
 int main() {
-    char s = "abcabcbb"
-    lengthOfLongestSubstring(s);
+    char s[] = "abcabcbb";
+    char ss[] = lengthOfLongestSubstring(s);
+    printf("The longest substring is: %s",ss);
     return 0;
  }
 
 int lengthOfLongestSubstring(char* s) {
-    int stringSize = sizeof(s) / sizeof(char);
-    char r[stringSize];
-    int i = 0;
+    int stringSize = strlen(s);
+    char r[10];
+    int i = 0, j = 0;
     //Check the current character with next
-    while (i < stringSize) {
+    while ((i < stringSize)&& (unq_ptrn(r, s[i], j))) {
         if (s[i] != s[i + 1]) {
             //If not same then add in an result array
-            r[i] = s[i];
-            r[i + 1] = s[i + 1];
-            ++i;
+            r[j] = s[i];
+            r[j + 1] = s[i + 1];
+            ++i; 
+            ++j;
         }
         else {
             //If same then update then increment the search index
             ++i;
         }
+        //Check if string pattern is unique or not  
     }
     return *r;
+}
+
+int unq_ptrn(char* r, char a, int crnt_indx) {
+    for (int k = 0; k < crnt_indx; k++)
+    {
+        if (*(r+k) != a)
+            continue;
+        else
+            return 0;
+    }
+    return 1;
 }
